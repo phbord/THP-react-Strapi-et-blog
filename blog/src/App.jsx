7b4774362d6b5b7a3ed94b6144507d4aa648d236
phbord <phbord@gmail.com>
2021-08-12 16:05:37 +0200

    maj

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,6 +1,20 @@
+import React, { useEffect } from 'react';
 import 'bootstrap';
 
 function App() {
+  React.useEffect(
+    () => {
+      fetch('http://localhost:1337/articles', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(response => response.json())
+        .then(data => console.log(data));
+    },
+    []
+  )
+
   return (
     <div className="App">
     </div>
